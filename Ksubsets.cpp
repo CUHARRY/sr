6d1c22d20fbdfa22b630d2d1539d00471a22c53d
CUHARRY <73715738+CUHARRY@users.noreply.github.com>
2020-10-31 00:02:25 +0530

    Add files via upload

---
+++ Ksubsets.cpp
@@ -0,0 +1,59 @@
+//partition a given set into 2 equal sum subsets
+#include<bits/stdc++.h>
+using namespace std;
+ bool backtrack(vector<int>& sums, vector<int>& nums, int target, int cur, int n, int k) 
+    { 
+        if(cur==n)
+        {
+            for(auto& sum:sums)
+            {
+                if(sum!=target)
+                    return false;
+            }
+            return true;
+        }
+        
+        for(int i=0;i<k;i++)
+        {   
+            if(sums[i]+nums[cur]>target)
+                continue;
+            
+            sums[i]+=nums[cur];
+            cout<<nums[cur]<<" "<<sums[i]<<"\n";
+            if(backtrack(sums,nums,target,cur+1,n,k))
+                    return true;
+            
+            sums[i]-=nums[cur];
+            cout<<"!!"<<nums[cur];
+            if(sums[i]==0)
+                return false;
+        
+        }
+        return false;
+
+    }
+
+bool CanSubset(vector<int> &vc, int k) {
+    int sum = accumulate(vc.begin(), vc.end(), 0);
+    if (sum % k != 0) return false;
+    sort(vc.rbegin(), vc.rend());
+
+    vector<int>sums(k, 0);
+
+    return backtrack(sums, vc, sum/k, 0, vc.size(), k);
+  }
+
+
+int main(){
+    int n; cin>>n;
+    vector<int> vc(n); 
+    for(int i=0;i<n;i++){
+        cin>>vc[i];
+    }
+
+    int k; cin>>k;//k is the number of ss.
+   cout<<endl<<CanSubset(vc,k);
+    
+    return 0;
+
+} 
